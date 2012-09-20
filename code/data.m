9c8119ed905be41c7dba215fbc78b71af33b0f5e
Aaku Kokko <aaku.kokko@gmail.com>
2012-09-20 16:41:58 +0200

    key requirements, available solar power

--- code/data.m
+++ code/data.m
@@ -1,6 +1,11 @@
-global solarConstant
-solarConstant = 1361
-global astronomicalUnit
-astronomicalUnit = 149597871
-jupiter.a = 778570000
-jupiter.e = 0.0489
+global solarConstant;
+solarConstant = 1361;
+global astronomicalUnit;
+astronomicalUnit = 149597871;
+global jupiter;
+jupiter.a = 778570000;
+jupiter.e = 0.0489;
+jupiter.mu = 126686534;
+global juno;
+juno.r = 1560.8;
+juno.mu = 3201;
--- code/minimumSolarIntensity.m
+++ code/minimumSolarIntensity.m
@@ -1,6 +1,6 @@
 function I = minimumSolarIntensity(planet)
-    global solarConstant
-    global astronomicalUnit
+    global solarConstant;
+    global astronomicalUnit;
     apoapsis = planet.a / (1 - planet.e);
     I = solarConstant * (astronomicalUnit / apohelion)^2;
 end
